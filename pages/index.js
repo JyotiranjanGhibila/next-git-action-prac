29df0ca742e846eff7949cad104c3b720c0696d8
JyotiranjanGhibila <ghibilamohan@gmail.com>
2023-01-08 16:03:13 +0530

    ag

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,4 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
-import GitProfile from "./components/GitProfile";
 
 const inter = Inter({ subsets: ["latin"] });
 
